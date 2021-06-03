f87e31f3c60f04fdec004cca34427a45a957e125
Iuliia <iuliiaponomareva@gmail.com>
2021-06-03 19:38:05 +0300

    Log exceptions

--- src/main/java/julia/productsapp/error/ErrorHandler.java
+++ src/main/java/julia/productsapp/error/ErrorHandler.java
@@ -1,5 +1,6 @@
 package julia.productsapp.error;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,20 +9,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
+@Log4j2
 public class ErrorHandler {
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    ResponseEntity<?> handleEmptyResult() {
+    ResponseEntity<?> handleEmptyResult(EmptyResultDataAccessException e) {
+        log.error(e.getMessage(), e);
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    ErrorRepresentation handleNoSuchElement() {
+    ErrorRepresentation handleNoSuchElement(NoSuchElementException e) {
+        log.error("Product not found", e);
         return new ErrorRepresentation(404, "No product with such id");
     }
 
     @ExceptionHandler(NoDataException.class)
     ErrorRepresentation handleNoData(NoDataException e) {
+        log.error(e.getMessage(), e);
         return new ErrorRepresentation(400, e.getMessage());
     }
 }
